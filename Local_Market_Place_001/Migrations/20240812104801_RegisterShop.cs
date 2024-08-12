1b7168664af84fd7e7c06b09c13a03cb1e414853
rushibhondave <rushikeshbhondave99@gmail.com>
2024-08-12 16:18:55 +0530

    Register Shop Datasbase is created

---
+++ Local_Market_Place_001/Migrations/20240812104801_RegisterShop.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Local_Market_Place_001.Migrations
+{
+    /// <inheritdoc />
+    public partial class RegisterShop : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RegisterShop",
+                columns: table => new
+                {
+                    ShopAddId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MobileNo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    ShopName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ShopTimings = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ShopCategory = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PinCode = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OwnerName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OwnerMobileNo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AadharNo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TermsAndConditionsAccepted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RegisterShop", x => x.ShopAddId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RegisterShop");
+        }
+    }
+}
